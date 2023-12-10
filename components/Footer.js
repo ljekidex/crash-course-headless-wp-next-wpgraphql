dffcf52d81c1c39d5cca495a50c087c73cdaee41
Dejan Ljevar <ljekidex@hotmail.com>
2023-12-10 17:53:27 +0100

    Fixing versions of React and other npms and Bug fixed regarding NextJS 14...

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,14 @@
-
-export default function Footer (){
-    return (
-        <footer>
-            <a
-            href="https://developers.wpengine.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            Part of the WP Engine Headless Developers Road Map 🗺️ 
-            </a>
-        </footer>
-    )
+import Link from "next/link";
+export default function Footer() {
+  return (
+    <footer>
+      <Link
+        href="https://developers.wpengine.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Part of the WP Engine Headless Developers Road Map 🗺️
+      </Link>
+    </footer>
+  );
 }
